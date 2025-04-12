ff69063eaf0022c7998afebe1c0178e4a682f564
ArminMolnar <molnar.armin9@gmail.com>
2025-04-13 00:17:01 +0200

    simplified fetching popular movies on home page

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,34 +1,15 @@
 import type {PageLoad} from './$types';
+import {fetchMovieData} from "$lib/api";
 
-export const load: PageLoad = async ({fetch}) => {
-    const API_KEY = '2403c84e57275967c9aee3f1f464845f';
-    const BASE_URL = 'https://api.themoviedb.org/3';
-
-    const totalPages = 2;
-    let allMovies: any[] = [];
-
+export const load: PageLoad = async () => {
     try {
-        for (let page = 1; page <= totalPages; page++) {
-            const response = await fetch(
-                `${BASE_URL}/movie/popular?api_key=${API_KEY}&language=hu-HU&page=${page}`
-            );
-
-            if (!response.ok) {
-                throw new Error(`Failed to fetch popular movies: ${response.statusText}`);
-            }
-
-            const data = await response.json();
-
-            console.log(`Fetched data from page ${page}:`, data);
-
-            allMovies = [...allMovies, ...data.results];
-        }
+        const data = await fetchMovieData('/movie/popular', 2);
 
         return {
-            popular: allMovies
+            popular: data
         };
     } catch (error) {
-        console.error('Error fetching popular movies:', error);
-        throw error;
+        console.error('Error in load function:', error);
+
     }
 };
