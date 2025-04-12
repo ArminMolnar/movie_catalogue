83b60f2105c67481ca6e47e5a97c6d519e2eaad2
ArminMolnar <molnar.armin9@gmail.com>
2025-04-12 23:05:49 +0200

    fixed error message

--- src/components/PopularMovies.svelte
+++ src/components/PopularMovies.svelte
@@ -1,14 +1,20 @@
 <script lang="ts">
-    export let popular: Array<{ title: string }>;
+    export let popular: Array<{
+        id: number;
+        title: string;
+        poster_path: string;
+        vote_average: number;
+        release_date: string;
+    }> = [];
     import MovieCard from "./MovieCard.svelte";
     console.log('PopularMovies received:', popular);
 </script>
 
 <h1>Most népszerű</h1>
 
-{#if popular}
+{#if popular && popular.length > 0}
     <div class="popular-movies">
-        {#each popular as movie}
+        {#each popular as movie (movie.id)}
             <MovieCard {movie}/>
         {/each}
     </div>
@@ -24,7 +30,7 @@
         grid-row-gap: 1rem;
     }
 
-    h1{
+    h1 {
         text-align: center;
         font-family: system-ui, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
     }
