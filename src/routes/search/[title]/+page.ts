1253ce1d2ae0c559f27f071fe5de01f309ad8720
ArminMolnar <molnar.armin9@gmail.com>
2025-04-12 23:25:42 +0200

    rename id -> title for better clarity

--- src/routes/search/[title]/+page.ts
+++ src/routes/search/[title]/+page.ts
@@ -1,5 +1,5 @@
 export async function load({fetch, params}){
-        const resp = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=2403c84e57275967c9aee3f1f464845f&language=hu-HU&query=${params.id}&page=1&include_adult=false`);
+        const resp = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=2403c84e57275967c9aee3f1f464845f&language=hu-HU&query=${params.title}&page=1&include_adult=false`);
 
         const data = await resp.json();
         console.log(data);
