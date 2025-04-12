c2ddb4f1e49e12c4c53ffe03a78112d33fc44f19
ArminMolnar <molnar.armin9@gmail.com>
2025-04-13 00:14:36 +0200

    display best movies

---
+++ src/routes/bestMovieList/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    import SearchMovies from '../../components/SearchMovies.svelte';
+    import MovieGrid from "../../components/MovieGrid.svelte";
+
+    export let data: { best: Array<{
+            id: number,
+            title: string,
+            poster_path: string,
+            vote_average: number,
+            release_date: string }> };
+
+    console.log('Page received data:', data);
+    const best = data.best;
+    console.log('Popular extracted:', best);
+</script>
+
+<section>
+    <SearchMovies/>
+    <MovieGrid movies={best} title="Legjobbra értékelt filmek" />
+</section>
