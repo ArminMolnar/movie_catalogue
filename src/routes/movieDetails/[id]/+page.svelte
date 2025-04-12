203e6b1f6fe2b9dd6aea9cecdcdb2bd32fecc924
ArminMolnar <molnar.armin9@gmail.com>
2025-04-12 23:03:19 +0200

    displaying movie details

---
+++ src/routes/movieDetails/[id]/+page.svelte
@@ -0,0 +1,62 @@
+<script lang="ts">
+    export let data;
+    const { movieDetails } = data;
+</script>
+
+    <div class="movie-details">
+        <div class="poster">
+            <img src={'https://image.tmdb.org/t/p/w500' + movieDetails.poster_path} alt="{movieDetails.title}" />
+        </div>
+        <div class="details">
+            <h1>{movieDetails.title}</h1>
+            <p><strong>Megjelenés dátuma:</strong> {movieDetails.release_date}</p>
+            <p><strong>Értékelés:</strong> {movieDetails.vote_average.toFixed(1)} <span style="color:gold; font-size: 1rem;">★</span></p>
+            <p><strong>Leírás:</strong> {movieDetails.overview}</p>
+            <a href="/" class="back-button">← Vissza</a>
+        </div>
+    </div>
+
+<style>
+    .movie-details {
+        display: flex;
+        flex-wrap: wrap;
+        gap: 2rem;
+        align-items: flex-start;
+        justify-content: center;
+        padding: 2rem;
+    }
+
+    .poster img {
+        width: 300px;
+        border-radius: 10px;
+    }
+
+    .details {
+        flex: 1;
+        max-width: 600px;
+    }
+
+    .details h1 {
+        margin-bottom: 1rem;
+    }
+
+    .details p {
+        margin-bottom: 1rem;
+        line-height: 1.5;
+        font-family: system-ui, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+    }
+
+    .back-button {
+        display: inline-block;
+        margin-top: 2rem;
+        text-decoration: none;
+        color: black;
+        font-weight: bold;
+        font-size: 1.6rem;
+        font-family: system-ui, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+    }
+
+    .back-button:hover {
+        text-decoration: underline;
+    }
+</style>
