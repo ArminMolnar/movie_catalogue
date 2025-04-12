423ceed77385245aab676184e1bb5a2920ae8018
ArminMolnar <molnar.armin9@gmail.com>
2025-04-12 17:28:00 +0200

    init

---
+++ src/routes/movieList/+page.svelte
@@ -0,0 +1,9 @@
+
+<style>
+    h2 {
+        margin-top: 20px;
+        text-align: center;
+    }
+</style>
+
+<h2>Filmek listája</h2>
---
+++ src/routes/watchList/+page.svelte
@@ -0,0 +1,8 @@
+<style>
+    h2 {
+        margin-top: 20px;
+        text-align: center;
+    }
+</style>
+
+<h2>Megnézendő filmek</h2>
