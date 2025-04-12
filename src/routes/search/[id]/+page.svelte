21bab65a4956181c57f0edf1d4f9575cc40a6450
ArminMolnar <molnar.armin9@gmail.com>
2025-04-12 23:03:09 +0200

    displaying search results

---
+++ src/routes/search/[id]/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    import MovieCard from "../../../components/MovieCard.svelte";
+
+    export let data;
+    const {searchedMovie} = data;
+    console.log('searchedMovie:', searchedMovie);
+</script>
+
+    <div class="searched-movies">
+        {#each searchedMovie as movie}
+            <MovieCard {movie}/>
+        {/each}
+    </div>
+
+<style>
+    .searched-movies {
+        display: grid;
+        grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+        gap: 1rem;
+        margin-top: 2rem;
+    }
+</style>
