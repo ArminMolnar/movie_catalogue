88f8d18afcf7a20dda8b1e26c556b0c5add02402
ArminMolnar <molnar.armin9@gmail.com>
2025-04-12 17:27:34 +0200

    create template for search bar

---
+++ src/components/SearchMovies.svelte
@@ -0,0 +1,42 @@
+<form class="search">
+    <label for="search_movie">Film keresése:</label>
+    <input name="search_movie" type="text" />
+    <button>Keresés</button>
+</form>
+
+<style>
+    button{
+        background-color: #555555;
+        border: none;
+        color: white;
+        padding: 6px 15px;
+        text-align: center;
+        font-family: system-ui, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+        text-decoration: none;
+        display: inline-block;
+        font-size: 14px;
+        border-radius: 10px;
+    }
+
+    input {
+        width: 20%;
+        border: none;
+        font-size: 1rem;
+        font-family: system-ui, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+        outline: none;
+        color: rgb(255, 255, 255);
+        background: rgb(63, 63, 63);
+        border-radius: 10px;
+        padding: 0.3rem 0.1rem;
+    }
+
+    label {
+        font-family: system-ui, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+        font-weight: bold;
+    }
+
+    .search{
+        margin-top: 1rem;
+        text-align: center;
+    }
+</style>
