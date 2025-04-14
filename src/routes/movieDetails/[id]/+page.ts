1ba0611087aa79bd00631d57684b2d83de67b014
ArminMolnar <molnar.armin9@gmail.com>
2025-04-15 00:43:36 +0200

    separated api_key from url (not final solution)

--- src/routes/movieDetails/[id]/+page.ts
+++ src/routes/movieDetails/[id]/+page.ts
@@ -1,8 +1,9 @@
 export async function load({ fetch, params }) {
-    const resp = await fetch(`https://api.themoviedb.org/3/movie/${params.id}?api_key=2403c84e57275967c9aee3f1f464845f&language=hu-HU`);
+    const API_KEY = '2403c84e57275967c9aee3f1f464845f';
+    const resp = await fetch(`https://api.themoviedb.org/3/movie/${params.id}?api_key=${API_KEY}&language=hu-HU`);
 
     const data = await resp.json();
-    console.log(data);
+
 
     if (resp.ok) {
         return {
--- src/routes/search/[title]/+page.ts
+++ src/routes/search/[title]/+page.ts
@@ -1,14 +1,20 @@
-export async function load({fetch, params}){
-        const resp = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=2403c84e57275967c9aee3f1f464845f&language=hu-HU&query=${params.title}&page=1&include_adult=false`);
 
-        const data = await resp.json();
-        console.log(data);
 
-        if(resp.ok){
-            return {
-                searchedMovie: data.results
-            }
+export async function load({fetch, params}) {
+    const API_KEY = '2403c84e57275967c9aee3f1f464845f';
+    const resp = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=hu-HU&query=${params.title}&page=1&include_adult=false`);
+
+    const data = await resp.json();
+
+    if (!params.title || params.title.trim().length < 2) {
+        return { searchedMovie: [] };
+    }
+
+    if (resp.ok) {
+        return {
+            searchedMovie: data.results
         }
+    }
 }
 
 
