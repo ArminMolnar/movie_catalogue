3671e00c4c264bf0eaa51c6738f7758320aba434
ArminMolnar <molnar.armin9@gmail.com>
2025-04-15 00:41:59 +0200

    used type Movie to simplify code

--- src/routes/bestMovieList/+page.svelte
+++ src/routes/bestMovieList/+page.svelte
@@ -1,20 +1,16 @@
 <script lang="ts">
     import SearchMovies from '../../components/SearchMovies.svelte';
     import MovieGrid from "../../components/MovieGrid.svelte";
+    import type {Movie} from "$lib/types";
 
-    export let data: { best: Array<{
-            id: number,
-            title: string,
-            poster_path: string,
-            vote_average: number,
-            release_date: string }> };
+    export let data: {
+        best: Movie[]
+    };
 
-    console.log('Page received data:', data);
     const best = data.best;
-    console.log('Popular extracted:', best);
 </script>
 
 <section>
     <SearchMovies/>
-    <MovieGrid movies={best} title="Legjobbra értékelt filmek" />
+    <MovieGrid movies={best} title="Legjobbra értékelt filmek"/>
 </section>
