4421be0149b5cef9221d490d2db70b84694ec17a
ArminMolnar <molnar.armin9@gmail.com>
2025-04-22 17:00:56 +0200

    added new types

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -7,3 +7,15 @@ export type Movie = {
     overview?: string;
     searchTerms?: string;
 };
+
+export interface Position {
+    x: number;
+    y: number;
+}
+
+export interface MenuItem {
+    name: string;
+    onClick?: () => void;
+    displayText?: string;
+    class?: string;
+}
