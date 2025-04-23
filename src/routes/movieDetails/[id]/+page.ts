4da8623a2e6a5600f19f88df97ea7aa14daa00e6
ArminMolnar <molnar.armin9@gmail.com>
2025-04-23 21:26:16 +0200

    included fecthing movie credits besides movie details

--- src/routes/movieDetails/[id]/+page.ts
+++ src/routes/movieDetails/[id]/+page.ts
@@ -1,14 +1,29 @@
-export async function load({ fetch, params }) {
-    const API_KEY = '2403c84e57275967c9aee3f1f464845f';
-    const resp = await fetch(`https://api.themoviedb.org/3/movie/${params.id}?api_key=${API_KEY}&language=hu-HU`);
+import { fetchMovie, fetchMovieCredits } from '$lib/api';
 
-    const data = await resp.json();
+export async function load({ params }) {
+    try {
+        const movieId = parseInt(params.id, 10);
 
+        const [movieDetails, movieCredits] = await Promise.all([
+            fetchMovie(movieId),
+            fetchMovieCredits(movieId)
+        ]);
 
-    if (resp.ok) {
+        const mainCast = movieCredits.cast
+            .filter((actor: { order: number; }) => actor.order <= 5)
+            .sort((a: { order: number; }, b: { order: number; }) => a.order - b.order);
+
         return {
-            movieDetails: data
+            movieDetails,
+            mainCast
+        };
+    } catch (error) {
+        console.error('Error loading movie details:', error);
+        return {
+            status: 500,
+            error: 'Failed to load movie details'
         };
     }
 }
+
 
