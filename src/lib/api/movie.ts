603faa5201e13c064b7c965c1b7be0815b132e03
ArminMolnar <molnar.armin9@gmail.com>
2025-04-27 23:11:23 +0200

    created functions to fetch movies from themoviedb.org

--- src/lib/api/movie.ts
+++ src/lib/api/movie.ts
@@ -1,12 +1,13 @@
 const API_KEY = '2403c84e57275967c9aee3f1f464845f';
-const BASE_URL = 'https://api.themoviedb.org/3';
+const BASE_URL = 'https://api.themoviedb.org/3/movie';
+const LANGUAGE='hu-HU';
 
-export async function fetchMovies(endpoint: string, totalPages: number = 1, language: string = 'hu-HU') {
+export async function fetchMovies(endpoint: string, totalPages: number = 1) {
     let allData: any[] = [];
 
     try {
         for (let page = 1; page <= totalPages; page++) {
-            const url = `${BASE_URL}${endpoint}?api_key=${API_KEY}&language=${language}&page=${page}`;
+            const url = `${BASE_URL}${endpoint}?api_key=${API_KEY}&language=${LANGUAGE}&page=${page}`;
             const response = await fetch(url);
 
             if (!response.ok) {
@@ -24,9 +25,9 @@ export async function fetchMovies(endpoint: string, totalPages: number = 1, lang
     }
 }
 
-export async function fetchMovie(movieId: number | string, language: string = 'hu-HU') {
+export async function fetchMovie(movieId: number | string) {
     try {
-        const url = `${BASE_URL}/movie/${movieId}?api_key=${API_KEY}&language=${language}`;
+        const url = `${BASE_URL}/${movieId}?api_key=${API_KEY}&language=${LANGUAGE}`;
         const response = await fetch(url);
 
         if (!response.ok) {
@@ -40,9 +41,9 @@ export async function fetchMovie(movieId: number | string, language: string = 'h
     }
 }
 
-export async function fetchMovieCredits(movieId: number, language: string = 'hu-HU') {
+export async function fetchMovieCredits(movieId: number) {
     try {
-        const url = `${BASE_URL}/movie/${movieId}/credits?api_key=${API_KEY}&language=${language}`;
+        const url = `${BASE_URL}/${movieId}/credits?api_key=${API_KEY}&language=${LANGUAGE}`;
         const response = await fetch(url);
 
         if (!response.ok) {
@@ -55,5 +56,3 @@ export async function fetchMovieCredits(movieId: number, language: string = 'hu-
         throw error;
     }
 }
-
-
