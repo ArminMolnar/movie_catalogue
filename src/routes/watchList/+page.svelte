4e74d2b8a40b0ed04667d8366dc886b63545916d
ArminMolnar <molnar.armin9@gmail.com>
2025-04-27 23:27:52 +0200

    improved logic, imported reusable components

--- src/routes/watchList/+page.svelte
+++ src/routes/watchList/+page.svelte
@@ -1,98 +1,58 @@
 <script lang="ts">
-    import { onMount } from 'svelte';
     import { watchlist } from '$lib/stores/movieStore';
     import MovieGrid from '../../components/MovieGrid.svelte';
-    import type { Movie } from '$lib/types';
-    import {goto} from "$app/navigation";
-    import {fetchMovie} from '$lib/api';
+    import type { Movie } from '$lib/types/movie';
+    import {fetchMovie} from '$lib/api/movie';
+    import HomeButton from "../../components/HomeButton.svelte";
+    import SectionTitle from "../../components/SectionTitle.svelte";
 
     let watchlistMovies: Movie[] = [];
-    let loading = true;
     let error: string | null = null;
 
-
-    $: watchlistIds = $watchlist;
-
-
     $: {
-        if (watchlistIds) {
-            fetchWatchlistMovies(watchlistIds);
+        if ($watchlist.length === 0) {
+            watchlistMovies = [];
+        } else {
+            fetchWatchlistMovies($watchlist);
         }
     }
 
     async function fetchWatchlistMovies(ids: number[]) {
-        if (ids.length === 0) {
-            watchlistMovies = [];
-            loading = false;
-            return;
-        }
-
-        loading = true;
         try {
             const promises = ids.map(id => fetchMovie(id));
             watchlistMovies = await Promise.all(promises);
             error = null;
         } catch (err) {
             console.error('Error fetching watchlist movies:', err);
-            error = 'Failed to load your watchlist. Please try again later.';
-        } finally {
-            loading = false;
+            error = 'Failed to load your watchlist. ';
         }
-    }
-
-    onMount(() => {
-        fetchWatchlistMovies($watchlist);
-    });
-
-    function goToHomePage() {
-        goto('/');
     }
 </script>
 
-<div class="watchlist-container">
-    <h1>Megnézendő filmek</h1>
+<title>Megnézendő filmek</title>
 
-    {#if loading}
-        <div class="loading">Loading your watchlist...</div>
-    {:else if error}
+<div class="watchlist-container">
+   <SectionTitle title={"Megnézendő filmek"}></SectionTitle>
+
+    {#if error}
         <div class="error">{error}</div>
-    {:else if watchlistMovies.length === 0}
+    {:else if $watchlist.length === 0}
         <div class="empty-list">
             <p>A megnézendő filmek listája üres.</p>
-            <p>Adj hozzá filmeket a "Megnézném" gombra kattintva a főoldalon.</p>
-            <button on:click={goToHomePage} class="main-page">← Főoldal</button>
+            <p>Adj hozzá filmeket a főoldalon.</p>
+            <HomeButton/>
         </div>
     {:else}
-        <MovieGrid movies={watchlistMovies}  inWatchlistView={true} />
+        <MovieGrid movies={watchlistMovies} inWatchlistView={true} />
     {/if}
 </div>
 
 <style>
-    h1 {
-        margin-top: 20px;
-        text-align: center;
-    }
-
     .empty-list {
         text-align: center;
-        font-family: system-ui, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
-    }
-
-    .main-page {
-        display: inline-block;
-        margin-top: 2rem;
-        text-decoration: none;
-        border-radius: 5px;
-        color: black;
-        font-weight: bold;
-        font-size: 1.6rem;
-        font-family: system-ui, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
-    }
-
-    .main-page:hover {
-        text-decoration: underline;
+        font-weight: 600;
     }
 </style>
 
-<title>Megnézendő filmek</title>
+
 
--- src/routes/watchedMovieList/+page.svelte
+++ src/routes/watchedMovieList/+page.svelte
@@ -1,100 +1,55 @@
 <script lang="ts">
-    import {onMount} from 'svelte';
     import {watchedMovies} from '$lib/stores/movieStore';
     import MovieGrid from '../../components/MovieGrid.svelte';
-    import type {Movie} from '$lib/types';
-    import {goto} from "$app/navigation";
-    import {fetchMovie} from '$lib/api';
+    import type {Movie} from '$lib/types/movie';
+    import {fetchMovie} from '$lib/api/movie';
+    import HomeButton from "../../components/HomeButton.svelte";
+    import SectionTitle from "../../components/SectionTitle.svelte";
 
-    let watchedMoviesList: Movie[] = [];
-    let loading = true;
+    let watchedMovieList: Movie[] = [];
     let error: string | null = null;
 
-
-    $: watchedIds = $watchedMovies;
-
-
     $: {
-        if (watchedIds) {
-            fetchWatchedMovies(watchedIds);
+        if ($watchedMovies.length === 0) {
+            watchedMovieList = [];
+        } else {
+            fetchWatchedMovies($watchedMovies);
         }
     }
 
     async function fetchWatchedMovies(ids: number[]) {
-        if (ids.length === 0) {
-            watchedMoviesList = [];
-            loading = false;
-            return;
-        }
-
-        loading = true;
         try {
-            const promises = ids.map(id =>
-                fetchMovie(id)
-            );
-            watchedMoviesList = await Promise.all(promises);
+            const promises = ids.map(id => fetchMovie(id));
+            watchedMovieList = await Promise.all(promises);
             error = null;
         } catch (err) {
-            console.error('Error fetching list of watched movies:', err);
-            error = 'Failed to load your list of watched movies. Please try again later.';
-        } finally {
-            loading = false;
+            console.error('Error fetching watched movies list movies:', err);
+            error = 'Failed to load your watched movies list.';
         }
-    }
-
-    onMount(() => {
-        fetchWatchedMovies($watchedMovies);
-    });
-
-    function goToHomePage() {
-        goto('/');
     }
 </script>
 
-<div class="watched-container">
-    <h1>Megnézett filmek</h1>
+<title>Megtekintett filmek</title>
 
-    {#if loading}
-        <div class="loading">Loading your watched movies list...</div>
-    {:else if error}
+<div class="watchedMovies-container">
+    <SectionTitle title={"Megtekintett filmek"}></SectionTitle>
+
+    {#if error}
         <div class="error">{error}</div>
-    {:else if watchedMoviesList.length === 0}
+    {:else if $watchedMovies.length === 0}
         <div class="empty-list">
-            <p>A megnézett filmek listája üres.</p>
-            <p>Adj hozzá filmeket a "Láttam" gombra kattintva a főoldalon.</p>
-            <button on:click={goToHomePage} class="main-page">← Főoldal</button>
+            <p>A megtekintett filmek listája üres.</p>
+            <p>Adj hozzá filmeket a főoldalon.</p>
+            <HomeButton/>
         </div>
     {:else}
-        <MovieGrid movies={watchedMoviesList} inWatchedView={true}/>
+        <MovieGrid movies={watchedMovieList} inWatchedView={true} />
     {/if}
 </div>
 
 <style>
-    h1 {
-        margin-top: 20px;
-        text-align: center;
-    }
-
     .empty-list {
         text-align: center;
-        font-family: system-ui, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
-    }
-
-    .main-page {
-        display: inline-block;
-        margin-top: 2rem;
-        text-decoration: none;
-        border-radius: 5px;
-        color: black;
-        font-weight: bold;
-        font-size: 1.6rem;
-        font-family: system-ui, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
-    }
-
-    .main-page:hover {
-        text-decoration: underline;
+        font-weight: 600;
     }
 </style>
-
-<title>Megnézett filmek</title>
-
