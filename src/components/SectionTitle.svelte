1c7de04346053a9e2659be495af4037f187fb664
ArminMolnar <molnar.armin9@gmail.com>
2025-04-27 23:12:19 +0200

    created reusable component to display header of pages

---
+++ src/components/SectionTitle.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    export let title;
+</script>
+
+<p class="section-title">{title}</p>
+
+<style>
+    .section-title {
+        text-align: center;
+        font-weight: 700;
+        font-size: 1.8rem;
+        margin: 1.5rem 0;
+        color: #082b36;
+        position: relative;
+    }
+</style>
