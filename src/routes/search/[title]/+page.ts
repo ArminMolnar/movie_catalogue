4d29c05ec52d37c0aad6daf162a32ca1a2e6ab5f
ArminMolnar <molnar.armin9@gmail.com>
2025-04-28 14:21:41 +0200

    imported api key, hiding it from public

--- src/routes/search/[title]/+page.ts
+++ src/routes/search/[title]/+page.ts
@@ -1,8 +1,8 @@
+import { PUBLIC_API_KEY } from '$env/static/public';
 export async function load({ fetch, params }) {
     try {
-        const API_KEY = '2403c84e57275967c9aee3f1f464845f';
         const resp = await fetch(
-            `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=hu-HU&query=${params.title}&page=1&include_adult=false`
+            `https://api.themoviedb.org/3/search/movie?api_key=${PUBLIC_API_KEY}&language=hu-HU&query=${params.title}&page=1&include_adult=false`
         );
 
         if (!params.title || params.title.trim().length < 2) {
