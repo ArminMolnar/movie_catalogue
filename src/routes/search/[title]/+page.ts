68365291205adb279d6fc28950b3bf33ab9c6633
ArminMolnar <molnar.armin9@gmail.com>
2025-04-28 13:03:33 +0200

    added error handling to load function

--- src/routes/search/[title]/+page.ts
+++ src/routes/search/[title]/+page.ts
@@ -1,18 +1,25 @@
-export async function load({fetch, params}) {
-    const API_KEY = '2403c84e57275967c9aee3f1f464845f';
-    const resp = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=hu-HU&query=${params.title}&page=1&include_adult=false`);
+export async function load({ fetch, params }) {
+    try {
+        const API_KEY = '2403c84e57275967c9aee3f1f464845f';
+        const resp = await fetch(
+            `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=hu-HU&query=${params.title}&page=1&include_adult=false`
+        );
 
-    const data = await resp.json();
-
-    if (!params.title || params.title.trim().length < 2) {
-        return { searchedMovie: [] };
-    }
-
-    if (resp.ok) {
-        return {
-            searchedMovie: data.results
+        if (!params.title || params.title.trim().length < 2) {
+            return { searchedMovie: [] };
         }
+
+        const data = await resp.json();
+
+        if (resp.ok) {
+            return {
+                searchedMovie: data.results
+            }
+        }
+    } catch (error) {
+        console.error('Error in load function:', error);
+        return {
+            searchedMovie: []
+        };
     }
 }
-
-
